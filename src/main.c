b544bcc6b8a9c2cc54cdb8cd01e71ccb99f4829a
milosz275 <miloszmaculewicz@gmail.com>
2024-10-29 10:13:43 +0100

    Fix init

--- src/main.c
+++ src/main.c
@@ -6,39 +6,22 @@
 #include "template.h"
 #include "lwipopts.h"
 
-#define TEMP_ADC 4
-
-const float conversion_factor = 3.3f / (1 << 12);
-
 int main()
 {
-    if (!stdio_init_all())
+    stdio_init_all();
+    if (cyw43_arch_init())
     {
-        printf("Stdio init failed");
-        return STDIO_INIT_FAILURE;
+        printf("Wi-Fi init failed");
+        return -1;
     }
-
-    gpio_init(CYW43_WL_GPIO_LED_PIN);
-    gpio_set_dir(CYW43_WL_GPIO_LED_PIN, GPIO_OUT);
-    gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
-
-    adc_init();
-    adc_gpio_init(26);
-    adc_set_temp_sensor_enabled(true);
-
-    adc_select_input(TEMP_ADC);
 
     while (true)
     {
         cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
-        sleep_ms(1000);
-
+        sleep_ms(250);
         cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
-        sleep_ms(1000);
+        sleep_ms(250);
 
-        const float voltage = adc_read() * conversion_factor;
-        const float temperature = 27 - (voltage - 0.706) / 0.001721;
-
-        printf("Hello, world! The temperature is: %fc\n", temperature);
+        printf("Hello, world!\n");
     }
 }
